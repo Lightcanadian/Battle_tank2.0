4f6cc6a41b8bb72370a5f9ec2bbe61a857569309
Lightcanadian <lightcanadian@gmail.com>
2019-01-03 19:50:53 -0500

    First project commit

---
+++ Battle_tank2/Source/Battle_tank2/Battle_tank2GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Battle_tank2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANK2_API ABattle_tank2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
