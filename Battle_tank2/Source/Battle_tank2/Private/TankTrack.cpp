dce78d53773284adbfadb5ba9edf63e5e67b5add
Lightcanadian <lightcanadian@gmail.com>
2019-02-06 23:45:04 -0500

    Ep 191 -! doesn't move

--- Battle_tank2/Source/Battle_tank2/Private/TankTrack.cpp
+++ Battle_tank2/Source/Battle_tank2/Private/TankTrack.cpp
@@ -8,6 +8,12 @@ UTankTrack::UTankTrack()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
 void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
@@ -15,8 +21,14 @@ void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompo
 	//getcomponnentvelocity
 	//getrightvector
 
+	
+}
+
+void UTankTrack::ApplySidewayForce()
+{
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
 	auto SlippageSpeed = FVector::DotProduct(GetComponentVelocity(), GetRightVector());
-	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime *GetRightVector();
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
 	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
 	auto CorrectionForce = TankRoot->GetMass() * CorrectionAcceleration / 2;
 
@@ -25,8 +37,19 @@ void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompo
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	CurrentThottle = FMath::Clamp<float>(CurrentThottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DrivingTrack()
+{
+	auto ForceApplied = GetForwardVector() * CurrentThottle * TrackMaxDrivingForce;
 	auto Forcelocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, Forcelocation);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("track hiting the ground"));
+	ApplySidewayForce();
 }
--- Battle_tank2/Source/Battle_tank2/Public/TankTrack.h
+++ Battle_tank2/Source/Battle_tank2/Public/TankTrack.h
@@ -18,12 +18,24 @@ public:
 
 	UTankTrack();
 
+	virtual void BeginPlay() override;
+
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void ApplySidewayForce();
 
 	UFUNCTION(BlueprintCallable, Category = Input)
 		void SetThrottle(float Throttle);
 
+	void DrivingTrack();
+
 	//Max force per track in newton  (Force = mass * acceleration)
 	UPROPERTY(EditDefaultsOnly, Category = Setup)
 		float TrackMaxDrivingForce = 400000; //assum 40 tonnetank and 1g accelleration
+
+private:
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	float CurrentThottle = 0;
 };
